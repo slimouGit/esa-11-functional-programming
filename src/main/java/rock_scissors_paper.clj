166eca95a8dcc9a94d330f0f298f065d8ee300a4
Salim Oussayfi <box@slimou.net>
2022-01-16 09:50:27 +0100

    minor refactorings

--- src/main/java/rock_scissors_paper.clj
+++ src/main/java/rock_scissors_paper.clj
@@ -1,5 +1,9 @@
 (ns rock-scissors-paper)
 
+(def y "you")
+(def e "your enemy")
+(def c "choose")
+
 (defn battle []
   (println "Choose your weapon: r = rock, s = scissors, p = paper")
   (let [weapon (read-line)]
@@ -15,27 +19,28 @@
       (= combo ["r" "p"]) 2
       (= combo ["s" "r"]) 2
       (= combo ["p" "s"]) 2)
-    ))
+    )
+  )
 
 (defn result [x]
   (cond
     (= x 0) (println "Undecided")
     (= x 1) (println "You loose")
     (= x 2) (println "You won"))
-    (println "valid result" (contains? #{1 2} x))
+  (println "valid result" (contains? #{1 2} x))
   )
 
 (defn evaluate [round]
-  (let [enemy (rand-nth ["r" "s" "p"])
-        you (battle)
+  (let [you (battle)
+        enemy (rand-nth ["r" "s" "p"])
         review (review enemy you)]
     (println "round" round)
-    (println "You choose" you)
-    (println "Your enemy choose" enemy)
-    (result review )
+    (cond (= you "r") (println y c "rock") (= you "s") (println y c "scissors") (= you "p") (println y c "paper"))
+    (cond (= enemy "r") (println e c "rock") (= enemy "s") (println e c "choose scissors") (= enemy "p") (println e c "choose paper"))
+    (result review)
     ))
 
 (loop [x 1]
   (when (< x 4)
     (evaluate x)
-    (recur(+ x 1))))
+    (recur (+ x 1))))
